9ace6efa509d76b0eec8817141c8b809f48c8b02
haimgia <giaoriana24@gmail.com>
2026-02-12 16:47:58 -0500

    extracted text from privacy policies

--- annotate_dataset.py
+++ annotate_dataset.py
@@ -6,10 +6,27 @@ import os
 from openai import OpenAI
 import pymupdf
 import re
+import asyncio
+from crawl4ai import AsyncWebCrawler
 
 load_dotenv()
 
+RAW_TEXT_FOLDER = "raw text"
+GEN_TRIPLES_FOLDER = "generated_triples"
+
 PDF_FOLDER = "scholarly_papers"
+PRIVACY_POLICY_FOLDER = "privacy_policies"
+
+PRIVACY_POLICY_SOURCES = {"Google Privacy Policy": "https://policies.google.com/privacy?hl=en",
+           "Microsoft Privacy Policy": "https://www.microsoft.com/en-us/privacy",
+           "SmartThings Privacy Policy": "https://eula.samsungiotcloud.com/legal/us/en/pps.html",
+           "Amazon Alexa and Echo Privacy Policy": "https://www.amazon.com/gp/help/customer/display.html?nodeId=GVP69FUJ48X9DK8V",
+           "Cisco Privacy Policy": "https://store.google.com/magazine/google_pixel_watch_compare?hl=en-US&pli=1",
+           "Apple Privacy Policy": "https://www.apple.com/legal/privacy/en-ww/",
+           "Huawei Privacy Policy": "https://consumer.huawei.com/en/privacy/privacy-statement-huawei/",
+           "Samsung Privacy Policy": "https://www.samsung.com/us/account/privacy-policy/",
+           "Ring Privacy Policy": "https://ring.com/privacy-notice?srsltid=AfmBOoo0ZH4T_iO5KXl9j1oenf05o819_C_nf95hTSg4BXXSmBgkJOyB",
+           "Schneider Electric": "https://www.se.com/us/en/about-us/legal/data-privacy/"}
 
 GPT_OSS_ENDPOINT = os.getenv("GPT_OSS_ENDPOINT")
 
@@ -87,10 +104,6 @@ def generate_triples_from_pdf(pdf_file):
         with open(triples_output, "w", encoding="utf-8") as g:
             for i, paragraph in tqdm(enumerate(paragraphs)):
 
-                # print(f"Paragraph {i+1}:")
-                # print(paragraph)
-                # print("-" * 20)
-
                 # writes the paragraph splits into the raw text file
                 f.write(f"{paragraph}\n\n")
 
@@ -159,10 +172,86 @@ def generate_triples(text):
     return triples
     #print(response.content)
 
+async def generate_triples_from_privacy_policies(source_name, url):
+     # makes the output file path
+    output_file = f"{source_name.replace(' ', '_')}.txt"
+
+    # create output directory if it doesn't exist
+    os.makedirs(os.path.join(RAW_TEXT_FOLDER, PRIVACY_POLICY_FOLDER), exist_ok=True)
+    os.makedirs(os.path.join(GEN_TRIPLES_FOLDER, PRIVACY_POLICY_FOLDER), exist_ok=True)
+
+    with open(os.path.join(RAW_TEXT_FOLDER, PRIVACY_POLICY_FOLDER, output_file), 'w', encoding='utf-8') as f:
+        f.write(f"Source: {source_name}\nURL: {url}\n\n")
+
+        async with AsyncWebCrawler() as crawler:
+            # Run the crawler on a URL
+            result = await crawler.arun(url)
+
+            # Strip markdown hyperlinks
+            text = re.sub(r"\[([^\]]+)\]\([^)]+\)", r"\1", result.markdown)
+
+            # Remove Markdown images: ![alt](url)
+            text = re.sub(r"!\[[^\]]*\]\([^)]+\)", "", text)
+
+            # Remove other hyperlinks
+            text = re.sub(r"\[[^\]]*\]\([^)]+\)", "", text)
+
+            # Remove empty/noisy lines
+            text = "\n".join(
+                line for line in text.splitlines()
+                if len(line.strip()) > 20
+            )
+
+            # Remove raw URLs
+            #text = re.sub(r"http[s]?://\S+", "", text)
+
+            sections = extract_sections(text)
+
+            for title, content in sections:
+                print("TITLE:", title)
+                print("CONTENT:", content)
+
+                f.write(f"{title}\n{content}\n\n")
+
+
+
+def extract_sections(markdown_text):
+    pattern = r"(#{1,3})\s+(.*)"
+
+    sections = []
+    current_title = "INTRO"
+    current_content = []
+
+    for line in markdown_text.splitlines():
+        match = re.match(pattern, line)
+
+        if match:
+            # Save previous section
+            if current_content:
+                sections.append((current_title, "\n".join(current_content).strip()))
+
+            # Start new section
+            current_title = match.group(2)
+            current_content = []
+        else:
+            current_content.append(line)
+
+    # Save last section
+    if current_content:
+        sections.append((current_title, "\n".join(current_content).strip()))
+
+    return sections
+    
+
 
     
 
 
 if __name__ == "__main__":
-    for pdf_file in tqdm(os.listdir(PDF_FOLDER)):
-        generate_triples_from_pdf(os.path.join(PDF_FOLDER, pdf_file))
+    # for pdf_file in tqdm(os.listdir(PDF_FOLDER)):
+    #     generate_triples_from_pdf(os.path.join(PDF_FOLDER, pdf_file))
+
+    # testing scraping with privacy policies
+    for source_name, url in PRIVACY_POLICY_SOURCES.items():
+        asyncio.run(generate_triples_from_privacy_policies(source_name, url))
+        
